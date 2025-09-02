542926ab221ebe077a129b49a1646c8438d2b6b7
Kevin Lewis <106688064+KLewis61@users.noreply.github.com>
2025-09-02 15:54:39 -0400

    Update killexisting.sh

--- killexisting.sh
+++ killexisting.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 APP_REF=$(curl --request POST --header 'content-type: application/json' --header "Authorization: Bearer $API_TOKEN" --url 'https://api.nowsecure.com/graphql' --data '{"query":"query Applications($platformTypes: [PlatformEnum!], $packageKeys: [String!], $groupRefs: [UUID!]) {\n  auto {\n    applications(platformTypes: $platformTypes, packageKeys: $packageKeys, groupRefs: $groupRefs) {\n      ref\n    }\n  }\n}","variables":{"platformTypes":"android","packageKeys":"com.klewis61.githubdemo","groupRefs":"3ee935d6-c347-4c45-967b-5c8e443392e5"}}' | jq -r '.data.auto.applications[0].ref')
 
 echo "App Ref = $APP_REF"
